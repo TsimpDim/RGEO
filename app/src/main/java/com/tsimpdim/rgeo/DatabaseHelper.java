bf4afb20a1a1c3ba224f6d09e68a26708a92707f
TsimpDim <dimitristsimpitas@gmail.com>
2018-11-15 12:18:42 +0200

    Basic text generation via database

---
+++ app/src/main/java/com/tsimpdim/rgeo/DatabaseHelper.java
@@ -0,0 +1,27 @@
+package com.tsimpdim.rgeo;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteQueryBuilder;
+
+import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
+
+public class DatabaseHelper extends SQLiteAssetHelper {
+    private static final String DATABASE_NAME = "cities.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    public Cursor getRndCity() {
+
+        SQLiteDatabase database = getReadableDatabase();
+
+        Cursor cursor = database.rawQuery("SELECT name FROM cities15000 ORDER BY RANDOM() LIMIT 1;", null);
+        cursor.moveToFirst();
+        return cursor;
+
+    }
+}
--- app/src/main/java/com/tsimpdim/rgeo/MainActivity.java
+++ app/src/main/java/com/tsimpdim/rgeo/MainActivity.java
@@ -1,7 +1,19 @@
 package com.tsimpdim.rgeo;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.sql.SQLException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +21,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        TextView mainTextView = (TextView) findViewById(R.id.myImageViewText);
+        processNewCity(mainTextView);
+
+
     }
+
+    private void processNewCity(TextView curTextView){
+
+        DatabaseHelper dbHelper = new DatabaseHelper(this);
+        Cursor rndCityCur = dbHelper.getRndCity();
+        String rndCityName = rndCityCur.getString(rndCityCur.getColumnIndex("name"));
+
+        try {
+            curTextView.setText(new String(rndCityName.getBytes("ISO-8859-1"), "UTF-8"));
+        }catch(UnsupportedEncodingException e){
+            curTextView.setText(rndCityName);
+            Toast.makeText(this,"Text might be shown incorrectly", Toast.LENGTH_LONG);
+        }
+
+    }
+
 }
