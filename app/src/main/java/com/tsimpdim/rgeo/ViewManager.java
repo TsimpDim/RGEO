74ec1ebfdaaf547b4dff6c65088986a18b8be343
TsimpDim <dimitristsimpitas@gmail.com>
2019-01-02 12:03:07 +0200

    Add animation on the TextViews

--- app/src/main/java/com/tsimpdim/rgeo/ViewManager.java
+++ app/src/main/java/com/tsimpdim/rgeo/ViewManager.java
@@ -2,33 +2,51 @@ package com.tsimpdim.rgeo;
 
 import android.app.Activity;
 import android.content.Context;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 import java.io.UnsupportedEncodingException;
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
 import java.text.NumberFormat;
+import java.util.HashMap;
 import java.util.Locale;
 
 public class ViewManager {
 
     private Context ctx;
+
+    private HashMap<String, TextView> tViews;
+    
+    // We save cityNameView separately from the rest of the tViews
+    // That's because we want it to display instantly and without any sort of modification
+    // e.g animation etc. etc.
     private TextView cityNameView;
-    private TextView cityPopView;
-    private TextView countryCodeView;
-    private TextView countryLangView;
-    private TextView countryCurrView;
-    private TextView countryTmzView;
+
+    private AlphaAnimation fadeIn;
+    private AlphaAnimation fadeOut;
 
     public ViewManager(Context context){
         ctx = context;
+
         cityNameView = ((Activity)ctx).findViewById(R.id.myImageViewText);
-        cityPopView = ((Activity)ctx).findViewById(R.id.population);
-        countryCodeView = ((Activity)ctx).findViewById(R.id.country);
-        countryLangView = ((Activity)ctx).findViewById(R.id.language);
-        countryCurrView = ((Activity)ctx).findViewById(R.id.currency);
-        countryTmzView = ((Activity)ctx).findViewById(R.id.timezone);
+
+        tViews = new HashMap<>();
+        tViews.put("ciPop", (TextView)((Activity)ctx).findViewById(R.id.population));
+        tViews.put("coName", (TextView)((Activity)ctx).findViewById(R.id.country));
+        tViews.put("coLang", (TextView)((Activity)ctx).findViewById(R.id.language));
+        tViews.put("coCurr", (TextView)((Activity)ctx).findViewById(R.id.currency));
+        tViews.put("coTmz", (TextView)((Activity)ctx).findViewById(R.id.timezone));
+
+        fadeIn = new AlphaAnimation(0.0f, 1.0f);
+        fadeOut = new AlphaAnimation( 1.0f, 0.0f);
+
+        fadeIn.setDuration(200);
+        fadeOut.setDuration(200);
     }
 
     public void setViewsFromCity(City city){
@@ -51,20 +69,37 @@ public class ViewManager {
 
         // Build & set final population string
         String populationString = ((Activity)ctx).getResources().getString(R.string.city_population, formatter.format(city.getPopulation())); // Final string from strings.xml placeholder
-        cityPopView.setText(populationString);
+        tViews.get("ciPop").setText(populationString);
 
         // Country name
-        countryCodeView.setText(((Activity)ctx).getResources().getString(R.string.country_code, city.getCountryName(), city.getCountryCode()));
+        tViews.get("coName").setText(((Activity)ctx).getResources().getString(R.string.country_code, city.getCountryName(), city.getCountryCode()));
 
         // Timezone
-        countryTmzView.setText(((Activity)ctx).getResources().getString(R.string.timezone, city.getTimezone()));
+        tViews.get("coTmz").setText(((Activity)ctx).getResources().getString(R.string.timezone, city.getTimezone()));
 
         // Currency
-        countryCurrView.setText(((Activity)ctx).getResources().getString(R.string.currency, city.getCurrency(), city.getCurrencyCode(), city.getCurrencySymbol()));
+        tViews.get("coCurr").setText(((Activity)ctx).getResources().getString(R.string.currency, city.getCurrency(), city.getCurrencyCode(), city.getCurrencySymbol()));
 
         // Language
-        countryLangView.setText(((Activity)ctx).getResources().getString(R.string.language, city.getLanguage()));
+        tViews.get("coLang").setText(((Activity)ctx).getResources().getString(R.string.language, city.getLanguage()));
 
+        fadeOutViews();
+        fadeInViews();
+
+    }
+
+    public void fadeOutViews(){
+        for (TextView tView : tViews.values()) {
+            tView.clearAnimation();
+            tView.startAnimation(fadeOut);
+        }
+    }
+
+    public void fadeInViews(){
+        for (TextView tView : tViews.values()) {
+            tView.clearAnimation();
+            tView.startAnimation(fadeIn);
+        }
     }
 
 }
